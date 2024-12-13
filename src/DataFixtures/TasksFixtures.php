78240f0d08aa973e71b5c67f97206a705a41b345
meline-p <meline.pischedda@gmail.com>
2024-12-13 21:33:51 +0100

    DataFixtures

---
+++ src/DataFixtures/TasksFixtures.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Task;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class TasksFixtures extends Fixture implements DependentFixtureInterface
+{
+    private $counter = 1;
+
+    public function load(ObjectManager $manager): void
+    {
+        $this->createTask(
+            "Finaliser le rapport d'audit",
+            "Compléter la section sur la dette technique et relire le document avant soumission.",
+            $manager
+        );
+
+        $this->createTask(
+            "Réserver une salle de réunion",
+            "Vérifier les disponibilités et réserver une salle pour la présentation de lundi matin.",
+            $manager
+        );
+
+        $this->createTask(
+            "Corriger les bugs sur l'application",
+            "Résoudre les erreurs de connexion pour les utilisateurs non authentifiés.",
+            $manager
+        );
+
+        $this->createTask(
+            "Mettre à jour le serveur de production",
+            "Appliquer les derniers correctifs de sécurité et redémarrer les services.",
+            $manager
+        );
+
+        $this->createTask(
+            "Créer une maquette pour la page d'accueil",
+            "Proposer un nouveau design basé sur le feedback des utilisateurs.",
+            $manager
+        );
+
+        $this->createTask(
+            "Écrire une documentation utilisateur",
+            "Rédiger un guide sur les fonctionnalités principales de l'application.",
+            $manager
+        );
+
+        $this->createTask(
+            "Rechercher de nouveaux outils SEO",
+            "Analyser les options pour améliorer le référencement du site.",
+            $manager
+        );
+
+        $this->createTask(
+            "Optimiser la base de données",
+            "Identifier et supprimer les index inutilisés pour améliorer les performances.",
+            $manager
+        );
+
+        $this->createTask(
+            "Planifier une session de brainstorming",
+            "Organiser une réunion pour discuter des prochaines fonctionnalités.",
+            $manager
+        );
+
+        $this->createTask(
+            "Tester la nouvelle version mobile",
+            "Vérifier les compatibilités sur différents appareils et systèmes d'exploitation.",
+            $manager
+        );
+
+        $manager->flush();
+    }
+
+    public function createTask(string $title, string $content, ObjectManager $manager): Task
+    {
+        $task = new Task();
+        $task->setTitle($title);
+        $task->setContent($content);
+
+        if (rand(0, 1) === 1) {
+            $user = $this->getReference('usr-'.rand(1, 7), User::class);
+            $task->setAuthor($user);
+        } else {
+            $task->setAuthor(null);
+        }
+
+        if (rand(0, 1) === 1) {
+            $task->setIsDone(true);
+        } else {
+            $task->setIsDone(false);
+        }
+
+        $this->counter++;
+
+        $manager->persist($task);
+
+        return $task;
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            UsersFixtures::class,
+        ];
+    }
+}
---
+++ src/DataFixtures/UsersFixtures.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\String\Slugger\SluggerInterface;
+
+class UsersFixtures extends Fixture
+{
+    private $counter = 1;
+
+    public function __construct(
+        private UserPasswordHasherInterface $passwordEncoder,
+        private SluggerInterface $slugger
+    ) {
+    }
+
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Faker\Factory::create('fr_FR');
+
+        $roles = [
+            'user' => ["ROLE_USER"],
+            'admin' => ["ROLE_ADMIN","ROLE_USER"]
+        ];
+
+        $new_user = new User();
+        $new_user->setEmail('john@doe.com');
+        $new_user->setUsername('johndoe');
+        $new_user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+        $new_user->setPassword(
+            $this->passwordEncoder->hashPassword($new_user, 'secret')
+        );
+        $manager->persist($new_user);
+        $this->addReference('usr-1', $new_user);
+
+        $new_user = new User();
+        $new_user->setEmail('toto@toto.fr');
+        $new_user->setUsername('toto');
+        $new_user->setRoles(["ROLE_USER", "ROLE_ADMIN"]);
+        $new_user->setPassword(
+            $this->passwordEncoder->hashPassword($new_user, 'secret')
+        );
+        $manager->persist($new_user);
+        $this->addReference('usr-2', $new_user);
+
+
+        for ($usr = 3; $usr <= 7; $usr++) {
+            $user = new User();
+            $user->setEmail($faker->email);
+            $username = explode('@', $faker->email);
+            $user->setUsername($this->slugger->slug($username[0])->lower());
+
+            $randomRole = $roles[array_rand($roles)];
+            $user->setRoles($randomRole);
+
+            $user->setPassword(
+                $this->passwordEncoder->hashPassword($new_user, 'secret')
+            );
+
+            $manager->persist($user);
+
+            // ajouter une référence user
+            $this->addReference('usr-'.$usr, $user);
+            $this->counter++;
+        }
+
+        $manager->flush();
+    }
+}
